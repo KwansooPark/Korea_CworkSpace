c4b54a63b4576e5d9e4a402073ad0ac15e2e6c19
unknown <pks5752@naver.com>
2025-05-15 21:50:44 +0900

    pointer

---
+++ Pointer/Pointer/ChangeArray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//매개변수로 배열 포인터를 전달한다.
+void changeArray(int* ptr) //int a[]: 전달 가능
+{
+	ptr[1] = 50;
+}
+
+int main()
+{
+	int arr[] = { 10, 20, 30 };
+
+	changeArray(arr);	//int* ptr = arr
+
+	//arr[1] = 50;
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d\n", arr[i]);
+	}
+
+	return 0;
+}
+
